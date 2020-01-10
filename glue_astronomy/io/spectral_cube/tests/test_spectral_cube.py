1d8e6bbf237f063c23eaa0235cde37a5f7d21dd9
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-10 19:49:56 +0000

    Added a test to make sure that the CASA loader works

--- glue_astronomy/io/spectral_cube/tests/test_spectral_cube.py
+++ glue_astronomy/io/spectral_cube/tests/test_spectral_cube.py
@@ -1,25 +1,36 @@
 import os
+import pytest
 from glue.qglue import parse_data
-from glue.tests.helpers import requires_spectral_cube
 
 DATA = os.path.join(os.path.dirname(__file__), 'data')
 
 
-@requires_spectral_cube
-def test_identifier():
+def test_identifier_fits():
     from ..spectral_cube import is_spectral_cube
     assert is_spectral_cube(os.path.join(DATA, 'cube_3d.fits'))
 
 
-@requires_spectral_cube
-def test_reader():
+def test_identifier_casa():
+    pytest.importorskip('casatools')
+    from ..spectral_cube import is_spectral_cube
+    assert is_spectral_cube(os.path.join(DATA, 'cube_3d.image'))
+
+
+def test_reader_fits():
     from ..spectral_cube import read_spectral_cube
     data = read_spectral_cube(os.path.join(DATA, 'cube_3d.fits'))
     data['STOKES I']
     assert data.shape == (2, 3, 4)
 
 
-@requires_spectral_cube
+def test_reader_casa():
+    pytest.importorskip('casatools')
+    from ..spectral_cube import read_spectral_cube
+    data = read_spectral_cube(os.path.join(DATA, 'cube_3d.image'))
+    data['STOKES I']
+    assert data.shape == (2, 3, 4)
+
+
 def test_qglue():
     from spectral_cube import SpectralCube
     cube = SpectralCube.read(os.path.join(DATA, 'cube_3d.fits'))
